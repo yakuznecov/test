662ca10f6f6af1354dfd1784a5c7dece8e62cf94
yakuznecov <yakuznecov@mail.ru>
2021-11-28 22:46:44 +0300

    scroll

--- js/info.js
+++ js/info.js
@@ -102,7 +102,7 @@ inputTopSearch.placeholder = 'Актёры';
 
 // Метрики ----------------------------------------------------------------------------------------------------------------->
 let elem = document.querySelector('.main-header-bottom-left');
-elem.clientWidth, elem.clientHeight; // Ширина и высота с учетом паддингов, марджинов и прокрутки
+elem.clientWidth, elem.clientHeight; // Ширина и высота с учетом паддингов, марджинов за вычетом скролла
 elem.scrollTop; // Высота прокрутки содержимого блока
 elem.scrollLeft; // величина прокрутки содержимого в левую сторону блока, скроллингом можно управлять программно
 elem.scrollWidth; // Ширина содержимого с учетом прокрутки
@@ -113,8 +113,34 @@ elem.clientTop; // смещение сверху внутренней части
 elem.clientLeft; // смещение слева внутренней части области от края элемента, может быть border
 elem.offsetWidth; // Ширина элемента с учетом всех отступов
 elem.offsetHeight; // Высота элемента с учетом всех отступов
+window.innerWidth; // полная ширина окна браузера вместе с полосой прокрутки (если она есть)
+window.innerHeight; // полная высота окна браузера вместе с полосой прокрутки (если она есть)
+elem.scrollWidth, elem.scrollHeight; // ширина и высота всего html документа
+window.pageXOffset; // смещение документа в окне браузера по оси x
+window.pageYOffset; // смещение документа в окне браузера по оси y
+window.scrollBy(offX, offY); // Прокручивает страницу относительно ее текущего положения на смещения offX и offY пикселей
+window.scrollTo(pageX, pageY); // Прокручивает страницу до указанных координат pageX, pageY, работают после полной загрузки документа
+elem.scrollIntoView(true); // Элемент при прокрутке будет вверху браузера. Если false в скобках, то элемент будет внизу окна браузера
+document.body.style.overflow = 'hidden'; // скрывает скролл у страницы
+document.body.style.overflow = ''; // показывает скролл у страницы
+elem.getBoundingClientRect(); // полные координаты объекта
+let x = document.elementFromPoint(x, y); // что находится в данной позиции на странице
 
 // ------------------------------------------------------------------------------------------------------------------------->
+
+// Убирает overflow при клике и добавляет обратно при клике на кнопку закрыть ----------------------------------------------->
+const massActors = document.querySelector('.dropdown-text-js');
+const transferActorsCloseBtn = document.querySelector('.transfer-actors-close-btn');
+
+massActors.addEventListener('click', function () {
+	mainItemsWrapper.style.overflow = 'inherit';
+});
+
+transferActorsCloseBtn.addEventListener('click', function () {
+	mainItemsWrapper.style.overflow = 'overlay';
+});
+
+// ------------------------------------------------------------------------------------------------------------------------>
 
 // Задачи
 
