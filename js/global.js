c13866714947eae18973a5197b58b8005afa124c
yakuznecov <yakuznecov@mail.ru>
2021-12-12 23:31:45 +0300

    spoilers

--- js/global.js
+++ js/global.js
@@ -1,1 +1,16 @@
-const
+const spoilerItemHeaders = document.querySelectorAll('.spoiler-item-header');
+
+spoilerItemHeaders.forEach((spoilerItemHeader) => {
+	spoilerItemHeader.addEventListener('click', (event) => {
+		const spoilerItem = spoilerItemHeader.parentNode;
+		spoilerItem.classList.toggle('active');
+		spoilerItemHeader.classList.toggle('active');
+		const spoilerItemBody = spoilerItemHeader.nextElementSibling;
+
+		if (spoilerItemHeader.classList.contains('active')) {
+			spoilerItemBody.style.maxHeight = spoilerItemBody.scrollHeight + 'px';
+		} else {
+			spoilerItemBody.style.maxHeight = 0;
+		}
+	});
+});
