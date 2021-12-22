2a64ce67d1496636a72658f8c54ae7db961c6fc8
yakuznecov <yakuznecov@mail.ru>
2021-12-22 16:42:23 +0300

    jq

--- js/jquery.js
+++ js/jquery.js
@@ -11,4 +11,75 @@ $(function () {
 	$('.points-grey-wrapper').mouseout(function () {
 		$('.points-grey-btn-header').removeClass('active');
 	});
-});
+
+	// Плавное появление меню. Hover на родителе. А меню display: none;
+	function showQuestionMenu() {
+		$('.dropdown-question').hover(
+			function () {
+				$('.question-menu').fadeIn(200);
+			},
+			function () {
+				$('.question-menu').fadeOut(100);
+			}
+		);
+	}
+
+	showQuestionMenu();
+
+	// выбор нечетных элементов img
+	var imgOdd = $('img:odd');
+
+	// выбор элементов
+	$(function () {
+		var jq = $('label');
+
+		// выбор первого элемента и воздействие на него
+		jq.first().css('border', 'thick double red');
+
+		// выбор последнего элемента и воздействие на него
+		jq.last().css('border', 'thick double green');
+
+		// выбор элемента с указанным индексом и воздействие на него
+		jq.eq(2).css('border', 'thick double black');
+		jq.eq(-2).css('border', 'thick double black');
+	});
+
+	// фильтрация элементов
+	$(function () {
+		// выборка элементов, в значении атрибута src которых
+		// содержится строка 'astor'
+		$('img').filter('[src*=astor]').css('border', 'thick double red');
+
+		// выборка элементов, содержащих строку 'daffodil'
+		var jq = $('[for*=daffodil]');
+		$('label').filter(jq).css('color', 'blue');
+
+		// выборка элементов, являющихся указанным элементом
+		var elem = document.getElementsByTagName('label')[1];
+		$('label').filter(elem).css('font-size', '1.5em');
+
+		// фильтрация элементов с использованием функции
+		$('img')
+			.filter(function (index) {
+				return this.getAttribute('src').indexOf('peony.png') > -1 || index == 5;
+			})
+			.css('border', 'thick solid red');
+	});
+
+	// Метод has()
+	$(function () {
+		$('div.dcell').has('img[src*=astor]').css('border', 'thick solid red');
+
+		var jq = $('[for*=p]');
+		$('div.dcell').has(jq).css('border', 'thick solid blue');
+	});
+
+	// Метод is()
+	// В этом сценарии мы проверяем, содержит ли объект jQuery элемент со значением атрибута src, который включает строку 'rose.png'.
+	$(function () {
+		var isResult = $('img').is(function (index) {
+			return this.getAttribute('src').indexOf('rose.png') != -1;
+		});
+		console.log('Результат: ' + isResult);
+	});
+})(jQuery);
