9aaf315c8026772d321e4c2dda63a7641cdf0ce0
yakuznecov <yakuznecov@mail.ru>
2022-01-19 11:30:38 +0300

    add game2

--- js/game2.js
+++ js/game2.js
@@ -11,15 +11,28 @@ const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
-// Стартовые значения
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
+let scores, currentScore, activePlayer, playing;
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+// Стартовые значения
+const init = function () {
+	scores = [0, 0];
+	currentScore = 0;
+	activePlayer = 0;
+	playing = true;
+
+	score0El.textContent = 0;
+	score1El.textContent = 0;
+	current0El.textContent = 0;
+	current1El.textContent = 0;
+
+	diceEl.classList.add('hidden');
+	player0El.classList.remove('player--winner');
+	player1El.classList.remove('player--winner');
+	player0El.classList.add('player--active');
+	player1El.classList.remove('player--active');
+};
+
+init();
 
 const switchPlayer = function () {
 	document.querySelector(`#current--${activePlayer}`).textContent = 0;
@@ -58,7 +71,7 @@ btnHold.addEventListener('click', function () {
 		document.querySelector(`#score--${activePlayer}`).textContent = scores[activePlayer];
 
 		// 2. Проверь, если счет игрока >= 100
-		if (scores[activePlayer] >= 20) {
+		if (scores[activePlayer] >= 100) {
 			// Если так, заканчивай игру
 			playing = false;
 			diceEl.classList.add('hidden');
@@ -70,3 +83,5 @@ btnHold.addEventListener('click', function () {
 		}
 	}
 });
+
+btnNew.addEventListener('click', init);
