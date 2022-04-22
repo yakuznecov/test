dd5f92b93cc64db4df818c80fb50d7dda2d735f8
yakuznecov~ <yakuznecov@mail.ru>
2022-04-22 22:17:25 +0300

    add test

---
+++ fotoAlbum/fotoAlbum.js
@@ -0,0 +1,31 @@
+let photos = [
+	// Sochi
+	'sochi1.jpg',
+	'sochi2.jpg',
+	'sochi3.jpg',
+	'sochi4.jpg',
+
+	// Moscow
+	'moscow1.jpg',
+	'moscow2.jpg',
+	'moscow3.jpg',
+	'moscow4.jpg',
+
+	// Piter
+	'piter1.jpg',
+	'piter2.jpg',
+	'piter3.jpg',
+	'piter4.jpg',
+
+	// Volgograd
+	'volgograd1.jpg',
+	'volgograd2.jpg',
+	'volgograd3.jpg',
+	'volgograd4.jpg',
+
+	// Habarovsk
+	'habarovsk1.jpg',
+	'habarovsk2.jpg',
+	'habarovsk3.jpg',
+	'habarovsk4.jpg',
+]
